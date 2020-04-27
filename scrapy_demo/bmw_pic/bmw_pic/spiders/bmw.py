a28e962965c331fbc20da479eedc4243f4afe5d2
ceaqw <2487689294@qq.com>
2020-04-27 20:13:18 +0800

    scrapy002

--- scrapy_demo/bmw_pic/bmw_pic/spiders/bmw.py
+++ scrapy_demo/bmw_pic/bmw_pic/spiders/bmw.py
@@ -12,6 +12,11 @@ class BmwSpider(scrapy.Spider):
     start_urls = ['https://car.autohome.com.cn/pic/series/65.html']
 
     def parse(self, response):
+        """
+        解析函数
+        :param response:
+        :return:
+        """
         uibox_list = response.xpath("//div[@class='uibox']")[1:]
         for uibox in uibox_list:
             print("进度--------->%d//%d" % (uibox_list.index(uibox)+1, len(uibox_list)))
@@ -21,4 +26,36 @@ class BmwSpider(scrapy.Spider):
             urls = [urllib.parse.urljoin(self.start_urls[0], image_url) for image_url in urls]
             # print(urls)
             item = BmwPicItem(part_name=part_name, urls=urls)
-            yield item
+            if uibox.xpath(".//li[@class='last']"):
+                yield scrapy.Request(
+                    url=urllib.parse.urljoin(self.start_urls[0], uibox.xpath(".//li[@class='last']/a/@href").get()),
+                    callback=self.get_detail
+                )
+            else:
+                yield item
+
+    def get_detail(self, response):
+        """
+        解析更多图片页面资源
+        :param response:
+        :return:
+        """
+        for url_page in response.xpath("//div[@class='page']//a/@href").getall()[3:-1]:
+            yield scrapy.Request(
+                url=urllib.parse.urljoin(self.start_urls[0], url_page),
+                callback=self.page_deal
+            )
+
+    def page_deal(self, response):
+        """
+        详情页处理函数
+        :param response:
+        :return:
+        """
+        print("-------->详情页加载")
+        part_name = response.xpath("//div[@class='uibox-title']/text()").getall()[-1]
+        urls = response.xpath("//div[contains(@class, 'uibox-con')]//li//img/@src").getall()
+        print(part_name)
+        item = BmwPicItem(part_name=part_name, urls=[urllib.parse.urljoin(self.start_urls[0], url) for url in urls])
+
+        yield item
